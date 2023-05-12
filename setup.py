1d6d6b393fe79b04f2eb884f104c7333fdbece3c
jsturt <j-sturt@outlook.com>
2023-05-12 14:45:44 +0100

    Updated dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(  
-  name = 'mandy',
-      version = '0.62',
-  author = 'John Sturt',
-  author_email = 'john.sturt.2018@live.rhul.ac.uk',
-  packages = find_packages(),
-  package_data = {'mandy':['atomic_data.csv', 'FormFactorData.dat']},
-  ) 
+if __name__ == '__main__':
+    setup()
+
+# setup(  
+#   name = 'mandy',
+#       version = '0.62',
+#   author = 'John Sturt',
+#   author_email = 'john.sturt.2018@live.rhul.ac.uk',
+#   url = 'https://github.com/jsturt/MANDY-Magnetic-Neutron-Diffraction',
+#   packages = find_packages(),
+#   package_data = {'mandy':['atomic_data.csv', 'FormFactorData.dat']},
+#   ) 
